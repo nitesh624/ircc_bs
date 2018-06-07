517ef38829c8fafa3b64365cdf55bec9787b6067
nitesh624 <nsinha624@gmail.com>
2018-06-07 13:03:18 +0530

    scroll to top button added

--- js/custom.js
+++ js/custom.js
@@ -75,6 +75,27 @@
 	})();
 
 
+
+	/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
+
+
 	// if(screen.width >= 768) {
 	// 	$('.navbar').addClass('navbar-fixed-top');
 	// 	$('body').css("margin-top", 0);
