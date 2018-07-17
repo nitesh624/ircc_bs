b6da6fe39387d5380cf2c481f7482af6e7420327
nitesh624 <nsinha624@gmail.com>
2018-07-17 13:21:31 +0530

    faculty areas of expertise slider added

--- includes/custom-scrollbar/scrollelement.js
+++ includes/custom-scrollbar/scrollelement.js
@@ -1,8 +1,7 @@
-!(function($){
+(function($){
     $(window).on("load",function(){
         $(".field-expertise-faculty ul").mCustomScrollbar({
-            theme: "dark-3",
-            scrollInertia: 810,
+            theme: "dark"
         });
     });
 })(jQuery);
